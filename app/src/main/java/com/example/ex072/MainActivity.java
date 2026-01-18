bcda39c6170502cd3bd5e361eb7bc7d9a979926d
itamar_jakubovitch <Itamarjakubovitch@gmail.com>
2026-01-18 13:59:22 +0200

    connecting the button

--- app/src/main/java/com/example/ex072/MainActivity.java
+++ app/src/main/java/com/example/ex072/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex072;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    ImageView iV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        iV = findViewById(R.id.iV);
+    }
+
+    public void clicked(View view)
+    {
 
     }
 }
